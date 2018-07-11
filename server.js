01864e1d471b5333747ec9ed53b949fb7036fb80
iivanovw7 <9iiiuk2@gmail.com>
2018-07-12 01:26:59 +0300

    initial commit

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
-const people = require('./people.json');
-const upcomings = require('./upcomings.json');
+
+const upcomings = require('./upcoming.json');
+const top_rated = require('./top_rated.json');
+const now_playing = require('./now_playing.json');
+const popular = require('./popular.json');
+const delay = require('delay');
+
 const app = express();
 const https = require('https');
-
+let getUpcomings = require('./upcomings.js');
 
 
 app.set('view engine', 'pug');
@@ -11,31 +16,82 @@ app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-    res.render('index', {
-        title: 'vidFlix.com',
-        people: people.profiles,
-        movies: upcomings.results,
-    });
+
+    try {
+        getUpcomings(1, 'upcoming')
+        res.render('index', {
+            title: 'vidFlix',
+            movies: upcomings.results,
+        })
+    } catch (err) {
+        console.log(err)
+    }
+
 });
 
+app.get('/toprated', (req, res) => {
 
-app.get('/profile', (req, res) => {
-    const person = people.profiles.find(p => p.id === req.query.id);
-    res.render('profile', {
-        title: `About ${person.firstname} ${person.lastname}`,
-        person,
-    });
+    try {
+        getUpcomings(1, 'top_rated')
+        res.render('index', {
+            title: 'vidFlix',
+            movies: top_rated.results,
+        })
+    } catch (err) {
+        console.log(err)
+    }
+
 });
+
+app.get('/popular', (req, res) => {
+
+    try {
+        getUpcomings(1, 'popular')
+        res.render('index', {
+            title: 'vidFlix',
+            movies: popular.results,
+        })
+    } catch (err) {
+        console.log(err)
+    }
+
+});
+
+app.get('/nowplaying', (req, res) => {
+
+    try {
+        getUpcomings(1, 'now_playing')
+        res.render('index', {
+            title: 'vidFlix',
+            movies: now_playing.results,
+        })
+    } catch (err) {
+        console.log(err)
+    }
+
+});
+
 
 app.get('/movie', (req, res) => {
-    console.log(upcomings);
+    //console.log(upcomings);
 
-    const SingleMovie = upcomings.results.find(p => p.id == req.query.id);
+    let SingleMovie = upcomings.results.find(p => p.id == req.query.id);
+
+    SingleMovie === undefined ?
+        SingleMovie = top_rated.results.find(p => p.id == req.query.id) :
+        SingleMovie === undefined ?
+            SingleMovie = popular.results.find(p => p.id == req.query.id) :
+            SingleMovie === undefined ?
+                SingleMovie = now_playing.results.find(p => p.id == req.query.id) :
+                console.log('Movie not found...');
     console.log(SingleMovie);
+
     res.render('movie', {
         title: `${SingleMovie.title}`,
         SingleMovie,
     });
+
+
 });
 
 
@@ -43,5 +99,7 @@ const server = app.listen(7070, () => {
     console.log(`Express running on PORT ${server.address().port}`);
     //console.log(tools.topCharts);
 });
+
+
 
 
--- upcomings.js
+++ upcomings.js
@@ -1,42 +1,21 @@
 const https = require('https');
 const fs = require('fs');
+const fetch = require('node-fetch');
+let Key = require('./apiKey');
 
+module.exports = function getUpcomings(page, index) {
 
-function getUpcomings() {
-
-    let movies = [];
-
-    formMoviesList = () => {
-
-        let object = JSON.parse(movies);
-        let data  = JSON.stringify(object, null, 2)
-
-        //fs.writeFile('upcomings.json', '', function(){console.log('Cleaning upcomings.json done.')});
-
-        fs.writeFile('upcomings.json', data,  function(){console.log('Writing in upcomings.json done.')});
-
-    };
-
-
-    https.get('https://api.themoviedb.org/3/movie/upcoming?api_key=c571ded9ad0c6a2890529f93abee07c4&language=ru&page=1', (res) => {
-
-        res.on('data', (d) => {
-            movies += d;
+    fetch(`https://api.themoviedb.org/3/movie/${index}?api_key=${Key.myKey()}&language=ru&page=${page}`)
+        .then(res => res.json())
+        .then(json => {
+            let data = JSON.stringify(json, null, 2);
+            fs.writeFile(`${index}.json`, data, function () {
+                console.log(`Writing data in ${index}.json done.`)
+                //console.log(`https://api.themoviedb.org/3/movie/${index}?api_key=${Key.myKey()}&language=ru&page=${page}`)
+            });
         });
 
-        res.on('end', () => {
-            formMoviesList()
-        });
-
-    }).on('error', (e) => {
-        console.error(e);
-    });
 
 };
-
-setInterval(function () {
-    getUpcomings();
-    console.log('Collecting data from api...')
-}, 48 * 60 * 60 * 1000);
 
 
